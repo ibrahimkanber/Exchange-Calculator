4279e19149ae4120326ce80cf4507754c7a6dd1a
ibrahimKanber <ikanber45@gmail.com>
2020-09-08 09:41:38 +0200

    als select value euro hinzufügt

--- script.js
+++ script.js
@@ -4,35 +4,42 @@ const selectRight = document.getElementById("selectRight")
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 
-input1.addEventListener("input", () => {
-    var rate = selectRight.value / selectLeft.value
-    console.log(rate * input1.value);
-    input2.value = rate * input1.value
-})
-
-selectLeft.addEventListener("change", () => {
-    var rate = selectRight.value / selectLeft.value
-    console.log(rate * input1.value);
-    input2.value = rate * input1.value
-
-})
-
-selectRight.addEventListener("change", () => {
-    var rate = selectRight.value / selectLeft.value
-    console.log(rate * input1.value)
-    input2.value = rate * input1.value
 
 
-})
 
+var euro;
 
+let rateCalculator = () => {
+    if (selectLeft.value == "euro" && selectRight.value != "euro") {
+        var rate = selectRight.value;
+        input2.value = rate * input1.value;
+    } else if (selectLeft.value == "euro" && selectRight.value == "euro") {
+        var rate = 1;
+        input2.value = rate * input1.value;
+    } else if (selectLeft.value !== "euro" && selectRight.value == "euro") {
+        var rate = 1 / selectLeft.value;
+        input2.value = rate * input1.value;
+    } else {
+        var rate = (selectRight.value) / selectLeft.value
+        console.log(rate * input1.value)
+        input2.value = rate * input1.value
+    }
+
+}
+
+input1.addEventListener("input", rateCalculator)
+
+selectLeft.addEventListener("change", rateCalculator)
+
+selectRight.addEventListener("change", rateCalculator)
 
 
 exchangeRate.then(res => res.json()).then(res => {
-    let html = "";
+    let html = "<option value='euro'>EUR</option>";
     for (let money in res.rates) {
         html += `<option value='${res.rates[money]}'>${money}</option>`
     };
+    console.log(res)
     selectLeft.innerHTML = html;
     selectRight.innerHTML = html;
 
